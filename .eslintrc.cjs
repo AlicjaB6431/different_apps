b61f340ab3724d1b4d30848f163c987cd4e85970
Szymon Jackiewicz <szymon.jackiewicz@wakacje.pl>
2023-06-03 13:56:27 +0200

    Dodanie reguły husky i reguł w eslilntrc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,21 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
+  overrides: [],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
-    'no-console': 'error',
+    'indent': ['error', 'tab'],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
     'no-unused-vars': ['error', { caughtErrors: 'all' }],
   },
 }
