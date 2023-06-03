8fa435873349a77fdb8bc7bf88a24345276feb69
Szymon Jackiewicz <szymon.jackiewicz@wakacje.pl>
2023-06-03 09:27:40 +0200

    Dodanie reguły husky i reguł w eslilntrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,5 +10,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'no-console': 'error',
+    'no-unused-vars': ['error', { caughtErrors: 'all' }],
   },
 }
